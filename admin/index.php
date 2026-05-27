725b703ca481a006aeb229eda7525431b0b84fc4
paracetamols69 <jaquaviontavius@proton.me>
2026-05-27 14:35:36 +0300

    fix being able to access admin panel if not admin

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["user_id"]) && $_SESSION["user_role"] != 3) {
+if (!isset($_SESSION["user_id"]) || $_SESSION["user_role"] != 3) {
     header("Location: /auth");
     exit;
 }
