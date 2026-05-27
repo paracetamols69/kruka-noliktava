ab246d1d992f6468e19a4c32e9e7e471ffd4fd67
Karalitis88 <pidrikis10@gmail.com>
2026-05-27 14:36:57 +0300

    MrRobot Protection

--- sorter/index.php
+++ sorter/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["user_id"]) && $_SESSION["user_role"] != 1) {
+if (!isset($_SESSION["user_id"]) || $_SESSION["user_role"] != 1) {
     header("Location: /auth");
     exit;
 }
--- worker/index.php
+++ worker/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["user_id"]) && $_SESSION["user_role"] != 2) {
+if (!isset($_SESSION["user_id"]) || $_SESSION["user_role"] != 2) {
     header("Location: /auth");
     exit;
 }
